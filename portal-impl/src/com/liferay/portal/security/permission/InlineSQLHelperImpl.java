51761724d7f2eff1aa45a11651bd1eea3e2259e4
ChrisKian <Christopher.Kian@liferay.com>
2016-11-30 17:22:27 -0800

    LPS-68104 SF

--- portal-impl/src/com/liferay/portal/security/permission/InlineSQLHelperImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/InlineSQLHelperImpl.java
@@ -338,8 +338,6 @@ public class InlineSQLHelperImpl implements InlineSQLHelper {
 
 		StringBundler sb = new StringBundler();
 
-		sb.append(StringPool.OPEN_PARENTHESIS);
-
 		sb.append("ResourcePermission.roleId IN (");
 
 		long[] roleIds = getRoleIds(groupIds);
@@ -370,8 +368,6 @@ public class InlineSQLHelperImpl implements InlineSQLHelper {
 				sb.append(StringPool.CLOSE_PARENTHESIS);
 			}
 		}
-
-		sb.append(StringPool.CLOSE_PARENTHESIS);
 
 		return sb.toString();
 	}
