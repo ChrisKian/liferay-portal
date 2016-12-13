93de49660119e95cc6857c8528a6aff9370a716c
ChrisKian <Christopher.Kian@liferay.com>
2016-12-13 11:53:57 -0800

    LPS-69516 Remove use of [$FALSE$] and SF

--- portal-impl/test/integration/com/liferay/portal/security/permission/InlineSQLHelperImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/security/permission/InlineSQLHelperImplTest.java
@@ -514,8 +514,6 @@ public class InlineSQLHelperImplTest extends InlineSQLHelperImpl {
 	}
 
 	private void _assertValidSql(String sql) throws Exception {
-		sql = StringUtil.replace(sql, "[$FALSE$]", "null");
-
 		try (Connection connection = DataAccess.getConnection();
 			PreparedStatement ps = connection.prepareStatement(sql)) {
 
