f6e2cad068a85b34a53258911a4b9dd69c545df2
ChrisKian <christopher.kian@liferay.com>
2018-01-30 13:00:47 -0800

    Try using site default language if possible.  If default, use just language;  don't include locale.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1405,7 +1405,8 @@ public class PortalImpl implements Portal {
 
 			if ((pos <= 0) || (pos >= groupFriendlyURL.length())) {
 				sb.append(groupFriendlyURL);
-				sb.append(buildI18NPath(siteDefaultLocale));
+				sb.append(
+					buildI18NPath(siteDefaultLocale, layout.getGroupId()));
 
 				if (!canonicalLayoutFriendlyURL.startsWith(StringPool.SLASH)) {
 					sb.append(StringPool.SLASH);
@@ -1418,7 +1419,8 @@ public class PortalImpl implements Portal {
 				String groupFriendlyURLSuffix = groupFriendlyURL.substring(pos);
 
 				sb.append(groupFriendlyURLPrefix);
-				sb.append(buildI18NPath(siteDefaultLocale));
+				sb.append(
+					buildI18NPath(siteDefaultLocale, layout.getGroupId()));
 				sb.append(groupFriendlyURLSuffix);
 			}
 
@@ -7445,10 +7447,10 @@ public class PortalImpl implements Portal {
 			companyId, groupId, 0, name, primaryKey, false, true, true);
 	}
 
-	protected String buildI18NPath(Locale locale) {
+	protected String buildI18NPath(Locale locale, long groupId) {
 		String languageId = LocaleUtil.toLanguageId(locale);
 
-		return _buildI18NPath(languageId, locale);
+		return _buildI18NPath(languageId, locale, groupId);
 	}
 
 	protected Set<Group> doGetAncestorSiteGroups(
@@ -8209,7 +8211,7 @@ public class PortalImpl implements Portal {
 			 !locale.equals(LocaleUtil.getDefault())) ||
 			(PropsValues.LOCALE_PREPEND_FRIENDLY_URL_STYLE == 2)) {
 
-			i18nPath = buildI18NPath(locale);
+			i18nPath = buildI18NPath(locale, themeDisplay.getSiteGroupId());
 		}
 
 		themeDisplay.setI18nLanguageId(locale.toString());
@@ -8217,21 +8219,22 @@ public class PortalImpl implements Portal {
 		themeDisplay.setLocale(locale);
 	}
 
-	private String _buildI18NPath(String languageId, Locale locale) {
+	private String _buildI18NPath(
+		String languageId, Locale locale, long groupId) {
+
 		if (Validator.isNull(languageId)) {
 			return null;
 		}
 
-		if (LanguageUtil.isDuplicateLanguageCode(locale.getLanguage())) {
-			Locale priorityLocale = LanguageUtil.getLocale(
-				locale.getLanguage());
+		Locale siteDefaultLocale = LanguageUtil.getLocale(
+			groupId, locale.getLanguage());
 
-			if (locale.equals(priorityLocale)) {
-				languageId = locale.getLanguage();
-			}
+		if (siteDefaultLocale == null) {
+			siteDefaultLocale = LanguageUtil.getLocale(locale.getLanguage());
 		}
-		else {
-			languageId = locale.getLanguage();
+
+		if (languageId.equals(LanguageUtil.getLanguageId(siteDefaultLocale))) {
+			languageId = siteDefaultLocale.getLanguage();
 		}
 
 		return StringPool.SLASH.concat(languageId);
@@ -8264,7 +8267,8 @@ public class PortalImpl implements Portal {
 
 		if (Validator.isNull(virtualHostname)) {
 			for (Locale locale : availableLocales) {
-				String i18nPath = buildI18NPath(locale);
+				String i18nPath = buildI18NPath(
+					locale, themeDisplay.getSiteGroupId());
 
 				alternateURLs.put(
 					locale,
@@ -8293,7 +8297,9 @@ public class PortalImpl implements Portal {
 		if ((pos <= 0) || (pos >= canonicalURL.length())) {
 			for (Locale locale : availableLocales) {
 				alternateURLs.put(
-					locale, canonicalURL.concat(buildI18NPath(locale)));
+					locale,
+					canonicalURL.concat(
+						buildI18NPath(locale, themeDisplay.getSiteGroupId())));
 			}
 
 			return alternateURLs;
@@ -8343,7 +8349,8 @@ public class PortalImpl implements Portal {
 		String canonicalURLSuffix = canonicalURL.substring(pos);
 
 		if (PropsValues.LOCALE_PREPEND_FRIENDLY_URL_STYLE == 2) {
-			String i18nPath = buildI18NPath(siteDefaultLocale);
+			String i18nPath = buildI18NPath(
+				siteDefaultLocale, layout.getGroupId());
 
 			if (canonicalURLSuffix.startsWith(i18nPath)) {
 				canonicalURLSuffix = canonicalURLSuffix.substring(
@@ -8392,8 +8399,10 @@ public class PortalImpl implements Portal {
 				alternateURLs.put(
 					locale,
 					canonicalURLPrefix.concat(
-						_buildI18NPath(languageId, locale)).concat(
-							alternateURLSuffix));
+						_buildI18NPath(
+							languageId, locale,
+							themeDisplay.getScopeGroupId())).concat(
+								alternateURLSuffix));
 			}
 		}
 
