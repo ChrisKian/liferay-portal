c23facd30d5d2b8217794f32e36c076a9acf5f80
ChrisKian <christopher.kian@liferay.com>
2018-05-23 14:58:56 -0700

    LPS-79309 Default locale should always trump available locales, regardless of ordering.

--- portal-impl/src/com/liferay/portal/language/LanguageImpl.java
+++ portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -1625,8 +1625,12 @@ public class LanguageImpl implements Language, Serializable {
 
 		String[] languageIds = PropsValues.LOCALES_ENABLED;
 
+		Locale defaultLocale = LocaleUtil.getDefault();
+
 		try {
 			Group group = GroupLocalServiceUtil.getGroup(groupId);
+
+			defaultLocale = PortalUtil.getSiteDefaultLocale(group);
 
 			UnicodeProperties typeSettingsProperties =
 				group.getTypeSettingsProperties();
@@ -1644,6 +1648,9 @@ public class LanguageImpl implements Language, Serializable {
 		HashMap<String, Locale> groupLanguageCodeLocalesMap = new HashMap<>();
 		HashMap<String, Locale> groupLanguageIdLocalesMap =
 			new LinkedHashMap<>();
+
+		groupLanguageCodeLocalesMap.put(
+			defaultLocale.getLanguage(), defaultLocale);
 
 		for (String languageId : languageIds) {
 			Locale locale = LocaleUtil.fromLanguageId(languageId, false);
@@ -1939,6 +1946,17 @@ public class LanguageImpl implements Language, Serializable {
 					languageIds = PropsValues.LOCALES_ENABLED;
 				}
 			}
+
+			Locale defaultLocale = LocaleUtil.getDefault();
+
+			String defaultLanguageId = LocaleUtil.toLanguageId(defaultLocale);
+
+			_languageCodeLocalesMap.put(
+				defaultLocale.getLanguage(), defaultLocale);
+
+			_languageIdLocalesMap.put(defaultLanguageId, defaultLocale);
+
+			languageIds = ArrayUtil.remove(languageIds, defaultLanguageId);
 
 			Set<String> duplicateLanguageCodes = new HashSet<>();
 
