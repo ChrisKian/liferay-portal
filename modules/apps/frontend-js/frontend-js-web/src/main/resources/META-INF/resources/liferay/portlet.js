7871fd37ba6de03b818c71f24b974625ef3fb203
ajsampang67 <ajsampang67@gmail.com>
2019-01-10 14:46:10 -0600

    LPS-88701 Revert "LPS-88701 Updated portlet location anchoring", fixing commit message for previous revert
    This reverts commit de704dc8f2cbc13de0521053a6f98bdcc9b143e5.

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/portlet.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/portlet.js
@@ -335,7 +335,7 @@
 					instance.refreshLayout(portletBound);
 
 					if (window.location.hash) {
-						window.location.href = window.location.hash;
+						window.location.hash = 'p_' + portletId;
 					}
 
 					portletBoundary = portletBound;
