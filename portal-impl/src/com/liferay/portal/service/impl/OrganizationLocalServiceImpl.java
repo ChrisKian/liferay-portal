3a1d1e79995470ef3ac5ad1c8c96a50e7a2f085d
drewbrokke <drew.brokke@liferay.com>
2019-05-21 17:33:04 -0700

    Revert "LPS-95837 Don't return empty or whitespace values."
    This reverts commit ad8d61c3a8bfc4c50bb509eedfd82c83ce2140bc.

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
@@ -509,22 +509,7 @@ public class OrganizationLocalServiceImpl
 
 	@Override
 	public String[] getChildrenTypes(String type) {
-		String[] childrenTypes = _organizationTypesSettings.getChildrenTypes(
-			type);
-
-		if (childrenTypes.length > 0) {
-			List<String> list = new ArrayList<>();
-
-			for (String childrenType : childrenTypes) {
-				if (Validator.isNotNull(childrenType)) {
-					list.add(childrenType);
-				}
-			}
-
-			childrenTypes = list.toArray(new String[0]);
-		}
-
-		return childrenTypes;
+		return _organizationTypesSettings.getChildrenTypes(type);
 	}
 
 	@Override
