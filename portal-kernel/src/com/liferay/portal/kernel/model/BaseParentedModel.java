e016bae151d50af57378da995f74e99aa060ab08
Joshua Cords <joshua.cords@liferay.com>
2020-10-16 18:01:17 -0700

    LPS-119230 portal-kernel: add BaseChildModel

--- portal-kernel/src/com/liferay/portal/kernel/model/BaseParentedModel.java
+++
@@ -1,9 +0,0 @@
-package com.liferay.portal.kernel.model;
-
-public interface BaseParentedModel {
-	public String getParentClassName();
-
-	public String getParentClassPK();
-
-	public String getTreePath();
-}
