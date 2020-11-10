8738938ab2cbf1b2d87e3b0c2ab68fe27f5a96a8
ChrisKian <christopher.kian@liferay.com>
2020-11-10 11:34:37 -0800

    No functional changes, just using an array format to pass export(s).

--- modules/apps/frontend-js/frontend-js-web/webpack.config.js
+++ modules/apps/frontend-js/frontend-js-web/webpack.config.js
@@ -16,26 +16,28 @@ const path = require('path');
 
 const PUBLIC_PATH = '/o/frontend-js-web/liferay/';
 
-module.exports = {
-	context: path.resolve(__dirname),
-	devtool: 'source-map',
-	entry: './src/main/resources/META-INF/resources/liferay/global.es.js',
-	mode: 'production',
-	module: {
-		rules: [
-			{
-				exclude: /node_modules/,
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
+module.exports = [
+	{
+		context: path.resolve(__dirname),
+		devtool: 'source-map',
+		entry: './src/main/resources/META-INF/resources/liferay/global.es.js',
+		mode: 'production',
+		module: {
+			rules: [
+				{
+					exclude: /node_modules/,
+					test: /\.js$/,
+					use: {
+						loader: 'babel-loader',
+					},
 				},
-			},
-		],
-	},
-	output: {
-		filename: 'global.bundle.js',
-		libraryTarget: 'window',
-		path: path.resolve('./build/node/packageRunBuild/resources/liferay/'),
-		publicPath: PUBLIC_PATH,
-	},
-};
+			],
+		},
+		output: {
+			filename: 'global.bundle.js',
+			libraryTarget: 'window',
+			path: path.resolve('./build/node/packageRunBuild/resources/liferay/'),
+			publicPath: PUBLIC_PATH,
+		},
+	}
+];
