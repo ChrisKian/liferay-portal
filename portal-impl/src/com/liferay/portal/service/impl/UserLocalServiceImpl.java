f67973fa446648773bb3196028dd39602718af6d
ChrisKian <christopher.kian@liferay.com>
2021-06-24 18:35:48 -0700

    LPS-134096 Use new methods and deprecate the old ones.

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -294,7 +294,10 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 * <code>admin.default.group.names</code>.
 	 *
 	 * @param userId the primary key of the user
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+	 * #addDefaultGroups(User)}
 	 */
+	@Deprecated
 	@Override
 	public void addDefaultGroups(long userId) throws PortalException {
 		addDefaultGroups(userPersistence.findByPrimaryKey(userId));
@@ -405,7 +408,10 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 * <code>admin.default.role.names</code>.
 	 *
 	 * @param userId the primary key of the user
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+	 * #addDefaultRoles(User)}
 	 */
+	@Deprecated
 	@Override
 	public void addDefaultRoles(long userId) throws PortalException {
 		addDefaultRoles(userPersistence.findByPrimaryKey(userId));
@@ -463,7 +469,10 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 * <code>admin.default.user.group.names</code>.
 	 *
 	 * @param userId the primary key of the user
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+	 * #addDefaultUserGroups(User)}
 	 */
+	@Deprecated
 	@Override
 	public void addDefaultUserGroups(long userId) throws PortalException {
 		addDefaultUserGroups(userPersistence.findByPrimaryKey(userId));
@@ -1305,7 +1314,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			groupLocalService.addUserGroups(userId, groups);
 		}
 
-		addDefaultGroups(userId);
+		addDefaultGroups(user);
 
 		// Organizations
 
@@ -1319,7 +1328,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			userPersistence.setRoles(userId, roleIds);
 		}
 
-		addDefaultRoles(userId);
+		addDefaultRoles(user);
 
 		// User groups
 
@@ -1327,7 +1336,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			userPersistence.setUserGroups(userId, userGroupIds);
 		}
 
-		addDefaultUserGroups(userId);
+		addDefaultUserGroups(user);
 
 		// Resources
 
