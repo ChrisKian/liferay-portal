8301dc7fcf36f6ded2c2c83382254d18e3d13836
ChrisKian <christopher.kian@liferay.com>
2022-01-28 14:16:39 -0800

    LPS-146149 Add UserModelListener class to reindex the SegmentsEntryRel entries if a user is updated

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/model/listener/UserModelListener.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.model.listener;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBus;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.segments.internal.constants.SegmentsDestinationNames;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Christopher Kian
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class UserModelListener extends BaseModelListener<User> {
+
+	@Override
+	public void onAfterUpdate(User originalUser, User user)
+		throws ModelListenerException {
+
+		_messageBus.sendMessage(
+			SegmentsDestinationNames.SEGMENTS_ENTRY_REL_INDEXER, new Message());
+	}
+
+	@Reference
+	private MessageBus _messageBus;
+
+}
