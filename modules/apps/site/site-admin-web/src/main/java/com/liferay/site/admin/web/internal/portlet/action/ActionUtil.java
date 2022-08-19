7332b21022dc719ca261c7f098100e3387880bff
ChrisKian <christopher.kian@liferay.com>
2022-08-19 10:27:09 -0700

    LPS-161024 Ensure virtual hostnames for layoutSets are always lowercase

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/action/ActionUtil.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/action/ActionUtil.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.sites.kernel.util.SitesUtil;
 
@@ -162,7 +163,8 @@ public class ActionUtil {
 				}
 			}
 
-			treeMap.put(virtualHostname, virtualHostLanguageId);
+			treeMap.put(
+				StringUtil.toLowerCase(virtualHostname), virtualHostLanguageId);
 		}
 
 		return treeMap;
