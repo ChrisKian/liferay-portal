bf0921dcdbb89c5ddddb93c344832503dff39b1e
ChrisKian <christopher.kian@liferay.com>
2024-07-11 17:04:30 -0700

    LPD-30298 Update test for full SAML workflow.  This may need to be squashed, or other commits may need to give test changes to this commit, not sure yet.

--- modules/test/playwright/tests/saml-web/saml.spec.ts
+++ modules/test/playwright/tests/saml-web/saml.spec.ts
@@ -10,7 +10,11 @@ import {applicationsMenuPageTest} from '../../fixtures/applicationsMenuPageTest'
 import {loginTest} from '../../fixtures/loginTest';
 import {samlAdminPagesTest} from '../../fixtures/samlAdminPagesTest';
 import {virtualInstancesPagesTest} from '../../fixtures/virtualInstancesPagesTest';
+import {ApiHelpers} from '../../helpers/ApiHelpers';
+import {liferayConfig} from '../../liferay.config';
+import {getRandomInt} from '../../utils/getRandomInt';
 import getRandomString from '../../utils/getRandomString';
+import performLogin, {performLogout} from '../../utils/performLogin';
 import {connectSpAndIdp} from './utils/samlProviderConnectionUtil';
 import {
 	createIdentityProviderVirtualInstance,
@@ -57,11 +61,9 @@ test('Create, edit, and delete a new virtual instance', async ({
 	await virtualInstancesPage.deleteVirtualInstance(name);
 });
 
-test('Create a new virtual instance, configure it for SAML IdP', async ({
-	identityProviderConnectionsPage,
+test('Create two virtual instances, one IdP and one SP, connect them, perform SP initialted SSO, perform SP initiated SLO', async ({
+	browser,
 	page,
-	samlAdminPage,
-	serviceProviderConnectionsPage,
 	virtualInstancesPage,
 }) => {
 
@@ -70,16 +72,22 @@ test('Create a new virtual instance, configure it for SAML IdP', async ({
 	const idpVirtualInstanceName = 'idp';
 
 	await createIdentityProviderVirtualInstance(
-	    idpVirtualInstanceName, idpVirtualInstanceName,
-		{page, samlAdminPage, virtualInstancesPage});
+		idpVirtualInstanceName,
+		idpVirtualInstanceName,
+		page
+	);
 
 	// Create new sp virtual instance
+
+	await page.goto('/');
 
 	const spVirtualInstanceName = 'sp';
 
 	await createServiceProviderVirtualInstance(
-	    spVirtualInstanceName, spVirtualInstanceName,
-		{page, samlAdminPage, virtualInstancesPage});
+		spVirtualInstanceName,
+		spVirtualInstanceName,
+		page
+	);
 
 	// Add a new connection for each provider, of the opposite provider
 
@@ -90,20 +98,130 @@ test('Create a new virtual instance, configure it for SAML IdP', async ({
 		undefined,
 		undefined,
 		undefined,
-		{
-			identityProviderConnectionsPage,
-			serviceProviderConnectionsPage,
-		}
+		page
 	);
 
 	// Next, attempt auth from SP, to IdP, then redirected back to SP
 
+	const defaultBaseUrl = liferayConfig.environment.baseUrl;
 
-	//Lastly, delete both virtual instances
+	liferayConfig.environment.baseUrl = `http://${idpVirtualInstanceName}:8080`;
+
+	// Create new page and apiHelper implementation based off IdP virtual instance
+
+	const idpVirtualInstancePage = await browser.newPage({
+		baseURL: liferayConfig.environment.baseUrl,
+	});
+
+	await performLogin(
+		idpVirtualInstancePage,
+		'test',
+		undefined,
+		`@${idpVirtualInstanceName}.com`
+	);
+
+	const idpApiHelpers = new ApiHelpers(idpVirtualInstancePage);
+
+	liferayConfig.environment.baseUrl = defaultBaseUrl;
+
+	// Create user in IdP instance
+
+	const userId = getRandomInt();
+
+	const user = await idpApiHelpers.headlessAdminUser.postUserAccount(
+		undefined,
+		userId
+	);
+
+	await performLogout(idpVirtualInstancePage);
+
+	liferayConfig.environment.baseUrl = `http://${spVirtualInstanceName}:8080`;
+
+	// Create new page and apiHelper implementation based off IdP virtual instance
+
+	const spVirtualInstancePage = await browser.newPage({
+		baseURL: `http://${spVirtualInstanceName}:8080`,
+	});
+
+	await performLogin(
+		spVirtualInstancePage,
+		'test',
+		'?p_p_id=com_liferay_login_web_portlet_LoginPortlet&p_p_state=maximized',
+		`@${spVirtualInstanceName}.com`
+	);
+
+	const spApiHelpers = new ApiHelpers(spVirtualInstancePage);
+
+	liferayConfig.environment.baseUrl = defaultBaseUrl;
+
+	// Create user in SP instance, using the same information as IdP user
+
+	await spApiHelpers.headlessAdminUser.postUserAccount(undefined, userId);
+
+	await performLogout(spVirtualInstancePage);
+
+	// Try to login as the new user from SP
+
+	await spVirtualInstancePage.goto('/');
+
+	const signInButton = await spVirtualInstancePage.getByRole('button', {
+		name: 'Sign In',
+	});
+
+	await signInButton.click();
+
+	// Verify we are being redirected to the IdP instance
+
+	await expect(
+		await spVirtualInstancePage.getByText(
+			'Redirecting to your identity provider...'
+		)
+	).toBeVisible();
+
+	await spVirtualInstancePage.waitForTimeout(5000);
+
+	// Verify we have been successfully redirected
+
+	await expect(await spVirtualInstancePage.url()).toContain(
+		idpVirtualInstancePage.url()
+	);
+
+	// Sign in
+
+	await spVirtualInstancePage
+		.getByLabel('Email Address')
+		.fill(user.emailAddress);
+	await spVirtualInstancePage.getByLabel('Password').fill('test');
+	await spVirtualInstancePage.getByLabel('Remember Me').check();
+	await spVirtualInstancePage.getByRole('button', {name: 'Sign In'}).click();
+
+	// Wait for authentication to complete, and verify we've been redirected back to SP
+
+	await spVirtualInstancePage.waitForTimeout(5000);
+
+	await expect(await spVirtualInstancePage.url()).toContain(
+		`http://${spVirtualInstanceName}:8080`
+	);
+
+	// Verify we are logged in
+
+	await expect(await page.getByTitle('User Profile Menu')).toBeVisible();
+
+	// Logout, verify we are also logged out of IdP
+
+	await performLogout(spVirtualInstancePage);
+
+	await idpVirtualInstancePage.goto('/');
+
+	await expect(
+		await idpVirtualInstancePage.getByRole('button', {name: 'Sign In'})
+	).toBeVisible();
+
+	// Lastly, delete both virtual instances
+
 	await virtualInstancesPage.deleteVirtualInstance(idpVirtualInstanceName);
 
 	await virtualInstancesPage.deleteVirtualInstance(spVirtualInstanceName);
 });
 
-test('testing', async ({page}) => {
-});
+test('testing', async ({page}) => {});
