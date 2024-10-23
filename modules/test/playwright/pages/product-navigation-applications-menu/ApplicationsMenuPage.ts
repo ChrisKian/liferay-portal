9c39689d4b2ebda9ef03017473b85da785b8d8c4
ChrisKian <christopher.kian@liferay.com>
2024-10-23 13:15:56 -0700

    LPD-37452 SF - Sort

--- modules/test/playwright/pages/product-navigation-applications-menu/ApplicationsMenuPage.ts
+++ modules/test/playwright/pages/product-navigation-applications-menu/ApplicationsMenuPage.ts
@@ -208,13 +208,13 @@ export class ApplicationsMenuPage {
 			exact: true,
 			name: 'System Settings',
 		});
-		this.usersAndOrganizationsItem = page.getByRole('menuitem', {
-			exact: true,
-			name: 'Users and Organizations',
-		});
 		this.userGroupsItem = page.getByRole('menuitem', {
 			exact: true,
 			name: 'User Groups',
+		});
+		this.usersAndOrganizationsItem = page.getByRole('menuitem', {
+			exact: true,
+			name: 'Users and Organizations',
 		});
 		this.virtualInstancesItem = page.getByRole('menuitem', {
 			exact: true,
@@ -465,6 +465,19 @@ export class ApplicationsMenuPage {
 		await this.serviceAccountsItem.click();
 	}
 
+	async goToUserGroups(forceReload = true) {
+		if (forceReload) {
+			await this.goto();
+		}
+		else {
+			await this.homePage.openApplicationMenu();
+
+			await expect(this.applicationsMenuTabButton).toBeVisible();
+		}
+		await this.controlPanelButton.click();
+		await this.userGroupsItem.click();
+	}
+
 	async goToUsersAndOrganizations(forceReload = true) {
 		if (forceReload) {
 			await this.goto();
@@ -483,18 +496,5 @@ export class ApplicationsMenuPage {
 		await this.goto();
 		await this.controlPanelButton.click();
 		await this.virtualInstancesItem.click();
-	}
-
-	async goToUserGroups(forceReload = true) {
-		if (forceReload) {
-			await this.goto();
-		}
-		else {
-			await this.homePage.openApplicationMenu();
-
-			await expect(this.applicationsMenuTabButton).toBeVisible();
-		}
-		await this.controlPanelButton.click();
-		await this.userGroupsItem.click();
 	}
 }
