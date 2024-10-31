bcc4ff829ffc613c308a3e7ea44726e2369258cb
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-10-31 20:18:22 -0300

    LPD-36309 SF, inline constants

--- modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserLocalServiceTest.java
+++ modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserLocalServiceTest.java
@@ -176,7 +176,7 @@ public class UserLocalServiceTest {
 		try (SafeCloseable safeCloseable =
 				_setLDAPAuthConfigurationWithSafeCloseable(true)) {
 
-			User user = _createUser(true, _INVALID_PASSWORD);
+			User user = _createUser(true, "abc");
 
 			Assert.assertEquals(
 				"User was created with incorrect LDAP Server Id", 1,
@@ -223,9 +223,9 @@ public class UserLocalServiceTest {
 		try (SafeCloseable safeCloseable =
 				_setLDAPAuthConfigurationWithSafeCloseable(false)) {
 
-			_assertUserPasswordException(true, _INVALID_PASSWORD);
+			_assertUserPasswordException(true, "abc");
 
-			_assertUserCreatedWithPasswordPolicy(true, _VALID_PASSWORD);
+			_assertUserCreatedWithPasswordPolicy(true, "Liferay123");
 		}
 		finally {
 			passwordPolicy.setChangeRequired(false);
@@ -252,9 +252,9 @@ public class UserLocalServiceTest {
 		try (SafeCloseable safeCloseable =
 				_setLDAPAuthConfigurationWithSafeCloseable(true)) {
 
-			_assertUserPasswordException(false, _INVALID_PASSWORD);
+			_assertUserPasswordException(false, "abc");
 
-			_assertUserCreatedWithPasswordPolicy(false, _VALID_PASSWORD);
+			_assertUserCreatedWithPasswordPolicy(false, "Liferay123");
 		}
 		finally {
 			passwordPolicy.setChangeRequired(false);
@@ -467,7 +467,7 @@ public class UserLocalServiceTest {
 		try (SafeCloseable safeCloseable =
 				_setLDAPAuthConfigurationWithSafeCloseable(true)) {
 
-			User user = _createUser(true, _VALID_PASSWORD);
+			User user = _createUser(true, "Liferay123");
 
 			_userLocalService.checkPasswordExpired(user);
 
@@ -500,7 +500,7 @@ public class UserLocalServiceTest {
 		try (SafeCloseable safeCloseable =
 				_setLDAPAuthConfigurationWithSafeCloseable(false)) {
 
-			User user = _createUser(true, _VALID_PASSWORD);
+			User user = _createUser(true, "Liferay123");
 
 			_userLocalService.checkPasswordExpired(user);
 
@@ -1616,10 +1616,6 @@ public class UserLocalServiceTest {
 				companyId, configurationProperties);
 		};
 	}
-
-	private static final String _INVALID_PASSWORD = "abc";
-
-	private static final String _VALID_PASSWORD = "Liferay123";
 
 	private static Company _company;
 
