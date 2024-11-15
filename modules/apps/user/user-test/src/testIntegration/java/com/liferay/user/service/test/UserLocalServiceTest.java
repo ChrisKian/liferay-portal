81d04e50bf32ccf7df0b13926c0acbcedc8798ed
ChrisKian <christopher.kian@liferay.com>
2024-11-14 17:58:24 -0800

    LPD-36309 SF on previously existing tests

--- modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserLocalServiceTest.java
+++ modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserLocalServiceTest.java
@@ -252,15 +252,13 @@ public class UserLocalServiceTest {
 					user.getCompanyId(), user.getEmailAddress(),
 					RandomTestUtil.randomString(), null, null, null));
 
-			try {
-				_userLocalService.authenticateByEmailAddress(
-					user.getCompanyId(), user.getEmailAddress(), password, null,
-					null, null);
-			}
-			catch (PortalException portalException) {
-				Assert.assertEquals(
-					PasswordExpiredException.class, portalException.getClass());
-			}
+			long companyId = user.getCompanyId();
+			String emailAddress = user.getEmailAddress();
+
+			AssertUtils.assertFailure(
+				PasswordExpiredException.class, null,
+				() -> _userLocalService.authenticateByEmailAddress(
+					companyId, emailAddress, password, null, null, null));
 
 			user = _userLocalService.fetchUser(user.getUserId());
 
@@ -743,16 +741,23 @@ public class UserLocalServiceTest {
 					user.getCompanyId(), user.getEmailAddress(),
 					RandomTestUtil.randomString(), null, null, null));
 
-			try {
-				_userLocalService.authenticateByEmailAddress(
-					user.getCompanyId(), user.getEmailAddress(), password, null,
-					null, null);
-			}
-			catch (PortalException portalException) {
-				Assert.assertEquals(
-					UserLockoutException.PasswordPolicyLockout.class,
-					portalException.getClass());
-			}
+			user = _userLocalService.getUser(user.getUserId());
+
+			long companyId = user.getCompanyId();
+			String emailAddress = user.getEmailAddress();
+
+			PasswordPolicy passwordPolicy = user.getPasswordPolicy();
+
+			String message = String.format(
+				"User %s was locked on %s by password policy %s and will be " +
+					"automatically unlocked on %s",
+				user.getUserId(), user.getLockoutDate(),
+				passwordPolicy.getName(), user.getUnlockDate(passwordPolicy));
+
+			AssertUtils.assertFailure(
+				UserLockoutException.PasswordPolicyLockout.class, message,
+				() -> _userLocalService.authenticateByEmailAddress(
+					companyId, emailAddress, password, null, null, null));
 
 			try {
 				_userLocalService.authenticateByEmailAddress(
@@ -845,8 +850,6 @@ public class UserLocalServiceTest {
 					PasswordEncryptorUtil.class,
 					"_PASSWORDS_ENCRYPTION_ALGORITHM", "SHA-384")) {
 
-			User user = UserTestUtil.addUser();
-
 			try (SafeCloseable safeCloseable =
 					_updateDefaultPasswordPolicyWithSafeCloseable(
 						passwordPolicy -> {
@@ -856,6 +859,8 @@ public class UserLocalServiceTest {
 
 				String password1 = "password1";
 				String password2 = "password2";
+
+				User user = UserTestUtil.addUser();
 
 				try {
 					ServiceContextThreadLocal.pushServiceContext(
@@ -891,10 +896,8 @@ public class UserLocalServiceTest {
 
 					Assert.fail();
 				}
-				catch (PortalException portalException) {
-					Assert.assertEquals(
-						UserPasswordException.MustNotBeRecentlyUsed.class,
-						portalException.getClass());
+				catch (UserPasswordException.MustNotBeRecentlyUsed
+							userPasswordException) {
 
 					Assert.assertEquals(
 						"{SHA-384}66b6aa56af08dc8caf7e001683058338244f436de61" +
@@ -1124,14 +1127,14 @@ public class UserLocalServiceTest {
 	public void testUnlockoutUserWithStaleLastFailedLoginDate()
 		throws Exception {
 
-		User user = UserTestUtil.addUser();
-
 		try (SafeCloseable safeCloseable =
 				_updateDefaultPasswordPolicyWithSafeCloseable(
 					passwordPolicy -> {
 						passwordPolicy.setLockout(false);
 						passwordPolicy.setResetFailureCount(3L);
 					})) {
+
+			User user = UserTestUtil.addUser();
 
 			long companyId = user.getCompanyId();
 			String emailAddress = user.getEmailAddress();
@@ -1159,8 +1162,6 @@ public class UserLocalServiceTest {
 
 	@Test
 	public void testUnlockoutUserWithStaleLockoutDate() throws Exception {
-		User user = UserTestUtil.addUser();
-
 		try (SafeCloseable safeCloseable =
 				_updateDefaultPasswordPolicyWithSafeCloseable(
 					passwordPolicy -> {
@@ -1168,6 +1169,8 @@ public class UserLocalServiceTest {
 						passwordPolicy.setMaxFailure(0);
 						passwordPolicy.setLockoutDuration(3L);
 					})) {
+
+			User user = UserTestUtil.addUser();
 
 			long companyId = user.getCompanyId();
 			String emailAddress = user.getEmailAddress();
