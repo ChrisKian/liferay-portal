31cf1c5fc2f1a92f59ff2eae7454d7b001346dcb
ChrisKian <christopher.kian@liferay.com>
2025-02-07 19:13:01 -0800

    move to test class

---
+++ modules/apps/portal-security/portal-security-ldap-test/src/testIntegration/java/com/liferay/portal/security/ldap/internal/exportimport/test/LDAPUserImporterImplTest.java
@@ -0,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.ldap.internal.exportimport.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.security.ldap.SafeLdapContext;
+import com.liferay.portal.security.ldap.SafePortalLDAP;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Christopher Kian
+ */
+@RunWith(Arquillian.class)
+public class LDAPUserImporterImplTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testApacheDS() throws Exception {
+		String baseProviderURL = "ldap://localhost:10389";
+		String principal = "uid=admin,ou=system";
+		String credentials = "secret";
+
+		SafeLdapContext safeLdapContext = _safePortalLDAP.getSafeLdapContext(
+			TestPropsValues.getCompanyId(), baseProviderURL, principal,
+			credentials);
+
+		if (safeLdapContext != null) {
+			Assert.fail("Found the correct mapping!");
+		}
+
+		baseProviderURL = "ldap://0.0.0.0:10389";
+
+		safeLdapContext = _safePortalLDAP.getSafeLdapContext(
+			TestPropsValues.getCompanyId(), baseProviderURL, principal,
+			credentials);
+
+		if (safeLdapContext != null) {
+			Assert.fail("Found the correct mapping!");
+		}
+	}
+
+	@Test
+	public void testOpenLDAP() throws Exception {
+		String baseProviderURL = "ldap://localhost:389";
+		String principal = "cn=admin,ou=test";
+		String credentials = "secret";
+
+		SafeLdapContext safeLdapContext = _safePortalLDAP.getSafeLdapContext(
+			TestPropsValues.getCompanyId(), baseProviderURL, principal,
+			credentials);
+
+		if (safeLdapContext != null) {
+			Assert.fail("Found the correct mapping!");
+		}
+
+		baseProviderURL = "ldap://0.0.0.0:389";
+
+		safeLdapContext = _safePortalLDAP.getSafeLdapContext(
+			TestPropsValues.getCompanyId(), baseProviderURL, principal,
+			credentials);
+
+		if (safeLdapContext != null) {
+			Assert.fail("Found the correct mapping!");
+		}
+	}
+
+	@Inject
+	private volatile SafePortalLDAP _safePortalLDAP;
+
+}
