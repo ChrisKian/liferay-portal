4c3ddccbbe78d9456f140f3a86628a20de28d536
ChrisKian <christopher.kian@liferay.com>
2025-02-25 20:52:01 -0800

    testing command line changes

---
+++ modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+CURRENT_DIR_NAME=$(dirname ${BASH_SOURCE[0]})
+
+echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
+
+source ${CURRENT_DIR_NAME}/../../../env/common.sh
+
+function main {
+	default_tear_down
+
+	simple_ldap_tear_down
+}
+
+function simple_ldap_tear_down {
+
+	# These steps will no longer be needed with next CI release
+	# Remove DB changes
+
+	# Find slapd.ldif file and count it's lines
+	local slapdLdif="/usr/local/etc/openldap/slapd.ldif"
+	local slapdwc="wc -l ${slapdLdif} | awk '{print $1}'"
+
+	# Count our DB definition lines
+	local databaseDefinition="${CURRENT_DIR_NAME}/slapd.ldif"
+	local ddwc="wc -l ${databaseDefinition} | awk '{print $1}'"
+	local diffwc="${slapdwc}-${ddwc}"
+
+	# Remove trailing database definition from slapd.ldif
+	head -n ${diffwc} ${slapdLdif} > temp.ldif && mv temp.ldif ${slapdLdif}
+
+	# Stop slapd service
+	kill -INT `cat /usr/local/var/run/slapd.pid`
+
+	# Test since dir might be different
+	echo "LDAP Test 3:"
+	if [ $? -ne 0 ]; then
+		echo "Command failed with exit status $?"
+		kill -INT `cat /usr/local/var/slapd.pid`
+	else
+		echo "Command succeeded"
+	fi
+
+	# Delete slapd.d dir (will need to be modified instead after release,
+	# or maybe just a file or two removed, not sure yet)
+
+	rm -rf /usr/local/etc/slapd.d
+}
+
+main "${@}"
