746f787a2e35d8aff746f834f0dca8e783fca0d1
ChrisKian <christopher.kian@liferay.com>
2025-02-25 20:52:01 -0800

    testing command line changes

--- modules/test/playwright/tests/portal-security-ldap/env/set_up.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/set_up.sh
@@ -6,21 +6,58 @@ echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
 
 source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
-function ldap_set_up {
+function simple_ldap_set_up {
 
-	local ldifFile="${CURRENT_DIR_NAME}/ldif.ldif"
+	# These steps will no longer be needed with next CI release
+	# Find slapd.ldif file
+	local slapdLdif="/usr/local/etc/openldap/slapd.ldif"
 
-	#Apache DS
-	/bin/bash -c ldapadd -c -D uid=admin,ou=system -f ${ldifFile} -H ldap://0.0.0.0:10389 -v -w secret -x
+	# Append our DB definition
+	local databaseDefinition="${CURRENT_DIR_NAME}/slapd.ldif"
 
-	#OpenLDAP
-	/bin/bash -c ldapadd -c -D cn=admin,ou=test -f ${ldifFile} -H ldap://0.0.0.0:389 -v -w secret -x
+	echo ${databaseDefinition} >> ${slapdLdif}
+
+	# Create slapd.d dir
+
+	mkdir /usr/local/etc/slapd.d
+
+	# Import configuration database
+
+	/usr/local/sbin/slapadd -n 0 -F /usr/local/etc/slapd.d -l ${slapdLdif}
+
+	# These steps will always be needed
+
+	# Start SLAPD
+	 /usr/local/libexec/slapd -F /usr/local/etc/slapd.d
+
+	# Test 1
+	echo "LDAP Test 1:"
+	if [ $? -ne 0 ]; then
+		echo "Command failed with exit status $?"
+	else
+		echo "Command succeeded"
+	fi
+
+	# Add user via ldif file
+	local ldifFile="${CURRENT_DIR_NAME}/simple.ldif"
+
+	ldapadd -x -D "cn=admin,dc=example,dc=com" -W -f ${ldifFile}
+
+	# Test 2
+	ldapsearch -x -b 'dc=example,dc=com' '(objectclass=*)'
+
+	echo "LDAP Test 2:"
+	if [ $? -ne 0 ]; then
+		echo "Command failed with exit status $?"
+	else
+		echo "Command succeeded"
+	fi
 }
 
 function main {
 	default_set_up
 
-	ldap_set_up
+	simple_ldap_set_up
 }
 
 main "${@}"
