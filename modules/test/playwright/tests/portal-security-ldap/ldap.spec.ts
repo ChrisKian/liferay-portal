0fe7b2bac2bc96954d3181ca2b539e23bb60c8c1
ChrisKian <christopher.kian@liferay.com>
2025-03-03 18:20:51 -0800

    wip

--- modules/test/playwright/tests/portal-security-ldap/ldap.spec.ts
+++ modules/test/playwright/tests/portal-security-ldap/ldap.spec.ts
@@ -5,33 +5,45 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
+import {instanceSettingsPagesTest} from '../../fixtures/instanceSettingsPagesTest';
+import {ldapConfigurationPagesTest} from '../../fixtures/ldapConfigurationPagesTest';
 import {loginTest} from '../../fixtures/loginTest';
-import {instanceSettingsPagesTest} from "../../fixtures/instanceSettingsPagesTest";
+import {TLdapServer} from '../../helpers/LdapConfigurationHelper';
+import getRandomString from '../../utils/getRandomString';
 
 export const test = mergeTests(
 	loginTest(),
-	instanceSettingsPagesTest
+	instanceSettingsPagesTest,
+	ldapConfigurationPagesTest
 );
 
-test('setting up LDAP server connection', async ({page, instanceSettingsPage}) => {
+test('setting up LDAP server connection', async ({
+	ldapConfigurationPage,
+	ldapServerPage,
+}) => {
+	const ldapServer: TLdapServer = {
+		defaultValues: 'OpenLDAP',
+		principal: 'cn=admin,dc=example,dc=com',
+		serverName: getRandomString(),
+	};
 
-	await instanceSettingsPage.goToInstanceSetting(
-		'LDAP',
-		'Servers',
-		false
-	);
+	await ldapServerPage.addLdapServer(ldapServer);
 
-	await instanceSettingsPage.page.getByRole('button', {name: 'Add'}).click();
-
-	await instanceSettingsPage.page.waitForTimeout(2000);
-
-	await instanceSettingsPage.page.getByLabel('Server Name Required').fill("test");
-
-	//testing OpenLDAP Server default with correct principal
-
-	await instanceSettingsPage.page.getByLabel('OpenLDAP').click();
-
-	await instanceSettingsPage.page.getByLabel('Principal').fill('cn=admin,dc=example,dc=com');
+	// await instanceSettingsPage.page.getByRole('button', {name: 'Add'}).click();
+	//
+	// await instanceSettingsPage.page.waitForTimeout(2000);
+	//
+	// await instanceSettingsPage.page
+	// 	.getByLabel('Server Name Required')
+	// 	.fill('test');
+	//
+	// // testing OpenLDAP Server default with correct principal
+	//
+	// await instanceSettingsPage.page.getByLabel('OpenLDAP').click();
+	//
+	// await instanceSettingsPage.page
+	// 	.getByLabel('Principal')
+	// 	.fill('cn=admin,dc=example,dc=com');
 
 	// await instanceSettingsPage.page.getByRole('button', { name: 'Test LDAP Connection' }).click();
 	//
@@ -59,9 +71,7 @@ test('setting up LDAP server connection', async ({page, instanceSettingsPage}) =
 	//
 	// await expect(false);
 
-
-//This is the next thing to get to, I need to build the LDAP page infrastructure,
+	// This is the next thing to get to, I need to build the LDAP page infrastructure,
 	// at least enough to test this issue cleanly, then a PR is ok.
-
 
 });
